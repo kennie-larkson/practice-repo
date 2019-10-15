420df270826ab9bf6752ceb1ec3254199317ffc9
kennie larkson <materialkenny@gmail.com>
2019-10-15 15:20:53 +0100

    created function Profile

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 
-let greeting = function(){
+function Profile(name, age, gender){
+     this.name = name;
+     this.age = age;
+     this.gender = gender;
 
-    console.log('This is a JavaScript greeting');
+
+     this.greeting = function(){
+
+        console.log('Hello there! I am '+this.name+' | '+this.age+' | '+this.gender);
+    }
+    
 }
+
+let person1 = new Profile('Lawal', 25, 'Male');
+person1.greeting();
+console.log(person1.name+' '+person1.age+' '+person1.gender);
+
+
+
