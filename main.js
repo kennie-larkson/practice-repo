7033ee4c476b1f8e5f185214a5bd9c404ca296bb
kennie larkson <materialkenny@gmail.com>
2019-10-15 15:26:47 +0100

    converted function to class

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 
-function Profile(name, age, gender){
-     this.name = name;
-     this.age = age;
-     this.gender = gender;
-
-
-     this.greeting = function(){
-
-        console.log('Hello there! I am '+this.name+' | '+this.age+' | '+this.gender);
+class Profile {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.greeting = function () {
+            console.log('Hello there! I am ' + this.name + ' | ' + this.age + ' | ' + this.gender);
+        };
     }
-    
 }
 
 let person1 = new Profile('Lawal', 25, 'Male');
